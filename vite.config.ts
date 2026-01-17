9d240c0e3dcd54bbb379a313d3a19a5c708f4e5e
Ramathakur <rama1998thakur@gmail.com>
2026-01-18 00:58:58 +0530

    fix Vite base for public deploy tesr

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',   // <-- Use './' for Vercel to work on any URL
+  base: './',   // <--- important for Vercel
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // fix @ imports
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 })
